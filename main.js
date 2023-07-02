0b702e955a619b9f553dca129794f76549082e2c
Hugo <swenkkros@gmail.com>
2023-07-03 01:50:52 +0200

    Add iframe and html, css, js text area

--- main.js
+++ main.js
@@ -1,24 +1,40 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const $ = (selector) => document.querySelector(selector);
 
-setupCounter(document.querySelector('#counter'))
+const $js = $("#js");
+const $css = $("#css");
+const $html = $("#html");
+
+$js.addEventListener("input", update);
+
+$css.addEventListener("input", update);
+
+$html.addEventListener("input", update);
+
+function update() {
+  const html = createHtml();
+  $("iframe").setAttribute("srcdoc", html);
+}
+
+const createHtml = () => {
+  const html = $html.value;
+  const css = $css.value;
+  const js = $js.value;
+  return `
+    <!DOCTYPE html>
+    <html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <style>
+            ${css}
+        </style>
+        <script>
+            ${js} 
+        </script>  
+    </head>
+    <body>
+        ${html}
+    </body>
+  `;
+};
